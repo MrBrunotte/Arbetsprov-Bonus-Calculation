c0b12e7b9352e571c0e15e3e7d316ed2300fca83
Stefan Brunotte <stefan.brunotte@gisys.se>
2022-05-10 09:20:06 +0200

    Code cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { BonusAppComponent } from './bonus-app.component';
 import { NavBarComponent } from './nav/navbar.component';
-import { KonsultComponent } from './konsult/konsult.component';
 import { BonusComponent } from './bonus/bonus.component';
-import { KonsultThumbnailComponent } from './konsult/konsult-thumbnail.components';
 import { StartsidaComponent } from './startsida/startsida.component';
-import { KonsultService } from './konsult/shared/konsult.service';
-import { SkapaNyKonsult } from './konsult/shared/skapa-nykonsult.component';
-import { KonsultDetailsComponent } from './konsult/konsult-details/konsult-details.component';
 import { Error404Component } from './errors/404.component'
-import { KonsultRouteActivator } from './konsult/konsult-details/konsult-route-activator.service';
 import { KonsultDashboardComponent } from './konsult-dashboard/konsult-dashboard.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -22,12 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     BonusAppComponent,
     NavBarComponent,
-    KonsultComponent,
     BonusComponent,
-    KonsultThumbnailComponent,
     StartsidaComponent,
-    SkapaNyKonsult,
-    KonsultDetailsComponent,
     Error404Component,
     KonsultDashboardComponent
   ],
@@ -37,21 +27,9 @@ import { HttpClientModule } from '@angular/common/http';
     ReactiveFormsModule,
     HttpClientModule
   ],
-  providers:
-    [
-      KonsultService,
-      {
-        provide: 'canDeactivateSkapaKonsult',
-        useValue: checkDirtyState
-      }
-  ],
+  providers:[],
   bootstrap: [BonusAppComponent]
 })
 export class AppModule { }
 
-export function checkDirtyState(component: SkapaNyKonsult): any {
-  if (component.isDirty) {
-    return window.confirm('Du har inte sparat den nya konsulten, vill du verkligen ångra?')
-    return true
-  }
-}
+
--- src/skapa-konsult/skapa-konsult.routes.ts
+++
@@ -1,5 +0,0 @@
-import { NyKonsultComponent } from './nykonsult.component';
-
-export const SkapaNyKonsultRoutes = [
-  { path: 'profile', component: NyKonsultComponent }
-]
